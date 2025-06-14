72de33f3ccb14e9f8be446ceff09d1d3bd5de70f
Priyanshu Bhardwaj <talkpriyanshusharma19@gmail.com>
2025-06-14 23:11:18 +0530

    Delete aws-setup/Terraform directory

--- aws-setup/Terraform/main.tf
+++
@@ -1,9 +0,0 @@
-provider "aws" {
-  region = "ap-south-1"
-}
-resource "aws_instance" "web" {
-  ami           = "ami-0f5ee92e2d63afc18"
-  instance_type = "t2.micro"
-  key_name      = "your-key-name"
-  tags = { Name = "RestaurantBookingServer" }
-}
